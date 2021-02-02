b225fae6c55940dda0ea43c6c7d02d7e07d7722b
duypk <khuongduy1484>
2021-02-02 10:37:06 +0700

    fix find not module request

--- src/app/service/authentication/request.interceptor.ts
+++ src/app/service/authentication/request.interceptor.ts
@@ -8,27 +8,28 @@ export class RequestInterceptor implements HttpInterceptor {
 
   constructor(
     private localService: LocalStorageService
-  ) { }
+  ) {
+  }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const accessToken = this.localService.accessToken;
     if (accessToken) {
-      request = request.clone({ headers: request.headers.set('Authorization', `Bearer ${accessToken}`) });
+      request = request.clone({headers: request.headers.set('Authorization', `Bearer ${accessToken}`)});
     }
 
     const ignore =
-        typeof request.body === 'undefined'
-        || request.body === null
-        || request.body.toString() === '[object FormData]';
+      typeof request.body === 'undefined'
+      || request.body === null
+      || request.body.toString() === '[object FormData]';
 
     if (ignore) {
       return next.handle(request);
     }
 
     if (!request.headers.has('Content-Type')) {
-        request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
+      request = request.clone({headers: request.headers.set('Content-Type', 'application/json')});
     }
 
-      return next.handle(request);
+    return next.handle(request);
   }
 }
